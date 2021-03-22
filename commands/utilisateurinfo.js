a5f83c70050c1a437e0ed5ce9c5771acd9fc4a93
DieuGinette <32375850+DieuGinette@users.noreply.github.com>
2021-03-22 18:09:44 +0100

    Add files via upload

---
+++ commands/utilisateurinfo.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'utilisateurinfo',
+    description: 'Renvoie le pseudo et le tag de la personne mentionné!',
+    execute(message, args) {
+        const user_mention = message.mentions.users.first();
+  message.channel.send(`Voici le tag de la personne mentionné: ${user_mention.tag}`)
+    }
+}
--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const fs = require("fs");
-const { Client } = require('discord.js');
+const { Client, Collection } = require('discord.js');
 const { TOKEN, PREFIX } = require('./config');
 const client = new Client();
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require (`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
   console.log(`Connecté en tant que ${client.user.tag}!`);
@@ -12,13 +21,8 @@ if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 const args = message.content.slice(PREFIX.length).split(/ +/);
 const command = args.shift().toLowerCase();
 
-if (command === 'ping') message.channel.send("Pong!")
-if (command === 'serveur') message.channel.send(`Tu es actuellement sur le serveur ${message.guild.name}`);
-if (command === 'utilisateur') message.channel.send(`Tu es l'utilisateur ${message.author.tag}`);
-if (command === 'utilisateurinfo') {
-  const user_mention = message.mentions.users.first();
-  message.channel.send(`Voici le tag de la personne mentionné: ${user_mention.tag}`)
-}
+if (!client.commands.has(command)) return;
+client.commands.get(command).execute(message, args);
 });
 
 client.login(TOKEN);
