b10f70215ba0eaeda15bbdf18eafb33df4986e12
DieuGinette <nathanblanchet@hotmail.com>
2021-04-10 12:22:58 +0200

    Creation de la commande deleterole

--- commands/addsroles.js
+++ commands/addsroles.js
@@ -1,17 +1,20 @@
 module.exports = {
-    name: 'addrole',
-    description: 'Ajouter un rôle!',
+    name: 'addsroles',
+    description: 'Ajouter plusieurs rôles!',
     execute(client, message, args) {
-        let role = message.guild.roles.cache.find(r => r.name === args.toString());
-        if (role) {
-            if (message.member.roles.cache.has(role.id)) return message.channel.send("Vous avez déjà ce rôle!");
-            if (role.permissions.has('KICK_MEMBERS')) return message.channel.send("Vous ne pouvez pas avoir ce rôle");
+        args.forEach(rName => {
+            let role = message.guild.roles.cache.find(r => r.name === rName.toString());
+            if (role) {
+                if (message.member.roles.cache.has(role.id)) return message.channel.send("Vous avez déjà ce rôle!");
+                if (role.permissions.has('KICK_MEMBERS')) return message.channel.send("Vous ne pouvez pas avoir ce rôle");
+    
+                message.member.roles.add(role)
+                .then(m => message.channel.send(`Vous possédez maintenant le rôle ${role}.`))
+                .catch(e => console.log(e));
+        } else {
+            message.channel.send("Le rôle n'existe pas!");
+         }
 
-            message.member.roles.add(role)
-            .then(m => message.channel.send(`Vous possédez maintenant le rôle ${role}.`))
-            .catch(e => console.log(e));
-    } else {
-        message.channel.send("Le rôle n'existe pas!");
-     }
+        })
     }
 }   
