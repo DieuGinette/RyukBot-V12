6f6a482b54692e376daa5f20f9237a15c853dd12
DieuGinette <nathanblanchet@hotmail.com>
2021-04-11 15:20:17 +0200

    Creation de la commande dire

---
+++ commands/Autre/dire.js
@@ -0,0 +1,8 @@
+module.exports.run = (client, message, args) => {
+    message.channel.send(args.join(" "));
+}
+
+module.exports.help = {
+    name: "dire",
+    description: "Répète le message d'un utilisateur !",
+};
