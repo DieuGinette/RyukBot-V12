b15f235c296ddf844b7926d8d99ca5de06803d46
DieuGinette <nathanblanchet@hotmail.com>
2021-06-21 18:56:49 +0200

    Ajout du kick dans le channel logs

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -1,8 +1,24 @@
+const { MessageEmbed } = require("discord.js")
+
 module.exports.run = (client, message, args) => {
     const user = message.mentions.users.first();
-    const reason = args.splice(1).join(' ');
+    const reason = (args.splice(1).join(' ') || 'Aucune raison spécifiée');
     user ? message.guild.member(user).kick(reason) : message.channel.send("L'utilisateur n'existe pas !");
-}
+
+    const embed = new MessageEmbed()
+    .setAuthor(`${user.id}`)
+    .setTitle('Un membre viens d\'être kick')
+    .setDescription(`Le membre **${user.username}** a été kick avec succès.`)
+    .setThumbnail(user.avatarURL())
+    .addField('Par', message.member, true)
+    .addField('Membre', user.username, true)
+    .addField('Raison', reason)
+    .setFooter("RyukBot", message.author.avatarURL())
+    .setTimestamp()
+    .setColor("#2f3136");
+
+    const log_channel = client.channels.cache.get('843055488534315019').send(embed);
+};
 
 module.exports.help = {
     name: "kick",
